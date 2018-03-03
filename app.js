525bea0af95b3dc9a864e3df31bc8047b2f7ba71
swssqewe12 <david@callanan.ie>
2018-03-03 15:26:04 +0000

    node_modules

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-const session = require('express-session');
+//const cookieParser = require('cookie-parser')
+//var bodyParser = require('body-parser')
+//const session = require('express-session');
 const app = express()
 
 app.use('/',       express.static('root'))
 app.use('/static', express.static('static'))
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({'secret': 'Ohhh... this is a secret msg gregory;;'}))
+//app.use(cookieParser())
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(session({'secret': 'Ohhh... this is a secret msg gregory;;'}))
 
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
